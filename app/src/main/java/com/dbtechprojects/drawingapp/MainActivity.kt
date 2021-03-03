fdc7bb2876fa02daaee6844089a113bccd1abce8
danielmbutler <danielmbutler@hotmail.co.uk>
2021-03-03 11:27:52 +0000

    app complete

--- app/src/main/java/com/dbtechprojects/drawingapp/MainActivity.kt
+++ app/src/main/java/com/dbtechprojects/drawingapp/MainActivity.kt
@@ -8,6 +8,8 @@ import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
+import android.media.MediaScannerConnection
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
@@ -21,6 +23,9 @@ import androidx.core.view.get
 import java.util.jar.Attributes
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.dialog_brush_size.*
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
 import java.lang.Exception
 import java.util.jar.Manifest
 
@@ -64,6 +69,12 @@ class MainActivity : AppCompatActivity() {
 
         ib_undo.setOnClickListener {
             drawing_view.onClickUndo()
+        }
+
+        ib_save.setOnClickListener {
+            if(isReadStorageAllowed()){
+                BitmapAsyncTask(getBitmapfromView(fl_drawing_view_container)).execute()
+            }
         }
     }
 
@@ -226,6 +237,84 @@ class MainActivity : AppCompatActivity() {
         val result = ContextCompat.checkSelfPermission(this, permission.READ_EXTERNAL_STORAGE)
 
         return result == PackageManager.PERMISSION_GRANTED
+    }
+
+    private inner class BitmapAsyncTask(val mBitmap: Bitmap): AsyncTask<Any, Void, String>(){
+
+        private lateinit var mProgressDialog : Dialog
+
+        override fun onPreExecute() {
+            super.onPreExecute()
+            ShowProgressDialog()
+        }
+
+        override fun doInBackground(vararg params: Any?): String {
+
+            var result = ""
+
+            if(mBitmap != null){
+                try {
+                    val bytes = ByteArrayOutputStream()
+                    mBitmap.compress(Bitmap.CompressFormat.PNG, 90,bytes)
+                    val f = File(externalCacheDir!!.absoluteFile.toString()
+                            + File.separator + "KidDrawingApp_" +
+                            System.currentTimeMillis() / 1000 + ".png")
+                    val fos = FileOutputStream(f)
+
+                    fos.write(bytes.toByteArray())
+                    fos.close()
+
+                    result = f.absolutePath
+
+                } catch (e: Exception){
+                    result =""
+                    e.printStackTrace()
+                }
+            }
+            return result
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            cancelProgressDialog()
+            if (!result!!.isEmpty()) {
+                Toast.makeText(
+                        this@MainActivity,
+                        "File saved successfully :$result",
+                        Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                Toast.makeText(
+                        this@MainActivity,
+                        "Something went wrong while saving the file.",
+                        Toast.LENGTH_SHORT
+                ).show()
+            }
+
+            MediaScannerConnection.scanFile(this@MainActivity, arrayOf(result), null){
+                path, uri -> val shareIntent = Intent()
+                shareIntent.action = Intent.ACTION_SEND
+                shareIntent.putExtra(Intent.EXTRA_STREAM, uri)
+                shareIntent.type = "image/png"
+
+                startActivity(
+                        Intent.createChooser(
+                                shareIntent,"Share")
+                )
+            }
+
+        }
+
+        private fun ShowProgressDialog(){
+            mProgressDialog = Dialog(this@MainActivity)
+            mProgressDialog.setContentView(R.layout.dialog_custom_progress)
+            mProgressDialog.show()
+        }
+
+        private fun cancelProgressDialog(){
+            mProgressDialog.dismiss()
+        }
+
     }
 
     companion object {
